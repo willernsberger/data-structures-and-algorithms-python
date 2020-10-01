83b4e82ee7ebe38fb5beb33b4bf94cc3c33a4a19
willernsberger@gmail.com <willernsberger@gmail.com>
2020-10-01 16:01:06 -0700

    Commit.

--- phone_numbers.py
+++ phone_numbers.py
@@ -1,12 +1,9 @@
 # given a list of "valid words" and a phone number of length k
 # return all the words that appear in the valid words list
-# which could have been contstructed from the phone number
+# which could have been constructed from the phone number
 
-
-
-
-
-
+# the phone map takes up constant space complexity
+# because it is at most 26 entries
 phone_map = {
     "a": 2, "b": 2, "c": 2,
     "d": 3, "e": 3, "f": 3,
@@ -18,4 +15,24 @@ phone_map = {
     "w": 9, "x": 9, "y": 9, "z": 9
 }
 
-print(phone_map["a"])
+# validate words is linear time complexity because it is a single
+# scan across the entries in the words list
+def validate_words(words, phone_number):
+    valid_words = []
+    # iterating across the list of words...
+    # adding the word to the valid words list if it checks out
+    for word in words:
+        word_number = []
+        for letter in word:
+            word_number.append(phone_map[letter])
+        if word_number == phone_number:
+            valid_words.append(word)
+    return valid_words
+
+
+# given inputs: list of words and a phone number
+words = ['dog', 'fish', 'cat', 'fog']
+phone_number = [3, 6, 4]
+
+# driver function call
+print(str(validate_words(words, phone_number)))
