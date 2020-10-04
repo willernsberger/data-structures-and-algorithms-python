780e2300a54de7db46455c47e5813beaed0a1467
willernsberger@gmail.com <willernsberger@gmail.com>
2020-10-03 22:51:49 -0700

    Commit.

---
+++ closest_points_to_origin.py
@@ -0,0 +1,38 @@
+import heapq
+import math
+
+# given an origin and  some points, find the k points closest to the origin
+
+
+def calculate_distance(point):
+    distance = point[0]*point[0] + point[1]*point[1]
+    distance = math.sqrt(distance)
+    return distance
+
+
+# O(nlog(n)) time for the sort
+def closest_points_to_origin(points, k):
+    # evaluating each point in the list of points
+    # sorted by the distance from the orgin
+    points = sorted(points, key = lambda x: calculate_distance(x))
+    return points[:k]
+
+
+def closest_points_to_origin_heap(points, k):
+    data = []
+    for p in points:
+        data.append((calculate_distance(p), p))
+    heapq.heapify(data)
+    result = []
+    for p in range(k):
+        result.append(heapq.heappop(data)[1])
+    return result
+
+# testing data
+k = 5
+points = [[-1, -5], [-1, 5], [5, 5], [6, 5],
+          [-3, 3], [3, 10], [-3, -3], [5, -4],
+          [10, -6], [-3, -8], [15, -2], [-10, 5]]
+
+# driver
+print(str(closest_points_to_origin_heap(points, k)))
