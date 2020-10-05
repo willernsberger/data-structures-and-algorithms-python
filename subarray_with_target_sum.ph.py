f5b229a1dde5f25934a80fbb0d4e191836d28b47
willernsberger@gmail.com <willernsberger@gmail.com>
2020-10-05 15:55:25 -0700

    Commit.

--- subarray_with_target_sum.ph.py
+++ subarray_with_target_sum.ph.py
@@ -13,12 +13,11 @@ def target_subarray_brute_force(arr, value):
             if subarray_sum == value:
                 subarray = [start, end]
                 return subarray
-
     return subarray
 
 
-
-def target_subarray_pointer_manipulation(arr, value):
+# O(n^2) time for the iteration and nested summation
+def target_subarray_pointer_sums(arr, value):
     subarray = []
 
     start, end = 0, 0
@@ -34,14 +33,33 @@ def target_subarray_pointer_manipulation(arr, value):
             end += 1
         elif subarray_sum > value:
             start += 1
-
     return subarray
 
 
+# O(n) time for the iteration
+def target_subarray_pointer_manipulation(arr, value):
+    start, end, = 0, 0
+    subarray_sum = arr[0]
+    while start < len(arr) and end < len(arr):
+        if subarray_sum == value:
+            return [start, end]
+        elif start == end and subarray_sum > value:
+            subarray_sum -= arr[start]
+            start += 1
+            end += 1
+            subarray_sum += arr[end]
+        elif subarray_sum < value:
+            end += 1
+            subarray_sum += arr[end]
+        elif subarray_sum > value:
+            subarray_sum -= arr[start]
+            start += 1
+    return []
+
 
 # testing
-arr = [1, 3, 2, 5, 7, 2, 4, 5, 9, 1, 4, 100]
+arr = [1, 3, 2, 5, 7, 2, 4, 5, 9, 1, 4, 99, 1]
 value = 100
 
 # driver
-print(target_subarray_brute_force(arr, value))
+print(target_subarray_pointer_manipulation(arr, value))
